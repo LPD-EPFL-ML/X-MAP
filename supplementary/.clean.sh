56de1e9077fa6e64824a562e2b5d4314fb1070c6
Rhicheek Patra <rhicheek.patra@epfl.ch>
2016-11-25 14:34:27 +0100

    Delete .clean.sh

--- supplementary/.clean.sh
+++
@@ -1,6 +0,0 @@
-files=( "aux" "bbl" "blg" "fdb_latexmk" "fls" "log" "synctex.gz" "dvi" "out" "toc" )
-
-for file in "${files[@]}"
-do
-    find . -path ./fig -prune -o -name "*.$file" -exec rm -rf '{}' \;
-done
